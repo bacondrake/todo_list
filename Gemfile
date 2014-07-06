f5b3d1cf1e422a4e5ebfb9c3abd44ca6205d2a83
bacondrake <frank.pavey@gmail.com>
2014-07-07 08:01:59 +1000

    Minor edits

--- Gemfile
+++ Gemfile
@@ -1,33 +1,17 @@
 source 'https://rubygems.org'
 ruby '2.1.0'
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.0'
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
-# Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 4.0.0'
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer',  platforms: :ruby
 
-# Use jquery as the JavaScript library
+gem 'rails', '4.1.0'
+gem 'sass-rails', '~> 4.0.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
-# bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'i18n', '0.6.9'
-
-# Bootstrap styling
 gem 'bootstrap-sass'
-
-# User controls
 gem 'devise'
-
-# Bootstrap pagination
 gem 'will_paginate-bootstrap'
 gem 'will_paginate', '~> 3.0'
 gem "twitter-bootstrap-rails"
@@ -36,7 +20,6 @@ group :development, :test do
      gem 'sqlite3'
 end
 
-# Production with Heroku
 group :production do
      gem 'pg'
      gem 'rails_12factor'
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,16 +3,12 @@ class TodosController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-  # GET /todos
-  # GET /todos.json
   def index
     # Paginates current_users todos, 
     # Puts into alphabetical order by content, then by whether it is completed or not
     @todos = current_user.todos.paginate(:page => params[:page], :per_page => 10).order('completed ASC, LOWER(content)')
   end
 
-  # GET /todos/1
-  # GET /todos/1.json
   def show
     # Only shows todos if they belong to the current user.
     if @todo.user == !current_user
@@ -20,18 +16,15 @@ class TodosController < ApplicationController
     end
   end
 
-  # GET /todos/new
   def new
     @todo = current_user.todos.build
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
-  # POST /todos
-  # POST /todos.json
   def create
+    # Creates new todo with date/time
     @todo = current_user.todos.build(todo_params)
     @todo.date_created = Time.new.strftime("%d %b %Y %H:%M")
     if @todo.content.blank?
@@ -43,8 +36,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
   def update
     if @todo.update(todo_params)
       redirect_to @todo, notice: 'Task was successfully updated.'
@@ -53,8 +44,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
     # Destroy for completed todos are 'cleared'
     if @todo.completed
@@ -88,27 +77,6 @@ class TodosController < ApplicationController
       end      
     end
     redirect_to todos_url, notice: 'Completed tasks have been cleared.'
-  end
-
-  # Delete all
-  # def delete_all
-  #   @delete_all = Todo.all
-  #   @delete_all.each do |todo|
-  #     todo.delete
-  #   end
-  #   redirect_to todos_url, notice: 'All todos have been deleted'
-  # end
-
-  def report
-    report = Array.new
-    @todos.each do |todo|
-      if todo.completed
-        report << todo
-        CSV.open('todos_report.csv', 'wb') do |csv|
-          csv << report
-        end
-      end
-    end
   end
 
   private
