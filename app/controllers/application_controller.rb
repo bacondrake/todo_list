eece123736de674de669a53ffdd3b1bc1197c35a
bacondrake <frank.pavey@gmail.com>
2014-07-10 20:22:13 +1000

    Added time_zone migration for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-    
-    def set_api_time_zone
-        utc_offset = current_user_session && current_user_session.user ? current_user_session.user.time_zone_offset.to_i.minutes : 0
-        user_timezone = ActiveSupport::TimeZone[utc_offset]
-        Time.zone = user_timezone if user_timezone
+  around_filter :set_time_zone
+
+  def set_time_zone
+    if current_user
+      Time.use_zone(current_user.time_zone) { yield }
+    else
+      yield
     end
+  end
 end
