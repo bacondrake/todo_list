bed2e933da98150f102d6feabed83fc3ad8125da
bacondrake <frank.pavey@gmail.com>
2015-03-16 16:43:00 +1100

    Started using simple_form and pagedown-bootstrap-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
+//= require pagedown_bootstrap
+//= require pagedown_init
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, { sort: column, direction: direction }, { class: css_class }
   end
+
+  # Defines the method for showing markdown formatting on the #show page.
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                          autolink: true,
+                                          space_after_headers: true,
+                                          fenced_code_blocks: true )
+    @markdown.render(content)
+  end
 end
